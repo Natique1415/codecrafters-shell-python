61a74d8a0e432cd2654751b60ff7579d87a99d3b
Natique Ibrar <ibrarnatique@gmail.com>
2026-05-09 23:42:30 +0400

    codecrafters submit [skip ci]

--- app/main.py
+++ app/main.py
@@ -5,8 +5,14 @@ def main():
     while True:
         sys.stdout.write("$ ")
         command = input().strip()
+
         if command == "exit":
             sys.exit()
+
+        elif command.startswith("echo "):
+            message = command[5:]
+            sys.stdout.write(f"{message}\n")
+
         else:
             sys.stdout.write(f"{command}: command not found\n")
 
