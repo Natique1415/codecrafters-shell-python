7882a3c04a0ee5d81c85844076f4622c56e21b24
Natique Ibrar <ibrarnatique@gmail.com>
2026-05-10 00:28:22 +0400

    codecrafters submit [skip ci]

--- app/main.py
+++ app/main.py
@@ -17,24 +17,35 @@ def main():
             sys.stdout.write(f"{message}\n")
 
         elif command.startswith("type "):
+
             if command[5:] in BUILTIN_COMMANDS:
                 sys.stdout.write(f"{command[5:]} is a shell builtin\n")
 
-            # now to check each dir in PATH
+            # now to check each dir in PATH for the program ( making sure it has executable permission )
             else:
-                directories = os.environ.get("PATH").split(os.pathsep)
+
+                def does_command_exist(command_name: str):
+                    directories = os.environ.get("PATH").split(os.pathsep)
+                    # command_name = command[5:].strip()
+                    # command_exist = False
+
+                    for directory in directories:
+                        full_path = os.path.join(directory, command_name)
+                        if os.path.isfile(full_path) and os.access(full_path, os.X_OK):
+                            return full_path
+                            # sys.stdout.write(f"{command_name} is {full_path}\n")
+                            # command_exist = True
+                            break
+                    return ("XXX",)
+                    # if command_exist == False:
+                    #     sys.stdout.write(f"{command_name}: not found\n")
+
                 command_name = command[5:].strip()
-                command_exist = False
-
-                for directory in directories:
-                    full_path = os.path.join(directory, command_name)
-                    if os.path.isfile(full_path) and os.access(full_path, os.X_OK):
-                        sys.stdout.write(f"{command_name} is {full_path}\n")
-                        command_exist = True
-                        break
-
-                if command_exist == False:
+                path_of_file = does_command_exist(command_name)
+                if path_of_file == ("XXX"):
                     sys.stdout.write(f"{command_name}: not found\n")
+                else:
+                    sys.stdout.write(f"{command_name} is {path_of_file}\n")
 
         else:
             sys.stdout.write(f"{command}: command not found\n")
