0c25d5dc4b85ce2d961b8a7079b5994e3becda62
Natique Ibrar <ibrarnatique@gmail.com>
2026-05-09 23:45:28 +0400

    codecrafters submit [skip ci]

--- app/main.py
+++ app/main.py
@@ -13,6 +13,13 @@ def main():
             message = command[5:]
             sys.stdout.write(f"{message}\n")
 
+        elif command.startswith("type "):
+            if command[5:] in ("echo","exit","type"):
+                sys.stdout.write(f"{command}: is a shell builtin\n")
+            else:
+                sys.stdout.write(f"{command}: not found\n")
+
+
         else:
             sys.stdout.write(f"{command}: command not found\n")
 
